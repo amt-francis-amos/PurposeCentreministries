fc13561882e57b17d682c85e24393b6d28f6b92c
amt-francis-amos <sugartone45@yahoo.com>
2024-06-29 12:10:52 -0700

    commit to main

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 
+let closeBtn = document.getElementById('closeBtn');
+let menuBtn = document.getElementById('menuBtn');
+let show = document.getElementById("nav-links");
+
+
+menuBtn.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
+
+
+function openMenu(){
+    
+    show.style.right = "0";
+}
+
+
+function closeMenu(){
+    
+    show.style.right = "-200px";
+}
